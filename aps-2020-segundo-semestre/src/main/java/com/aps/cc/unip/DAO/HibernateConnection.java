c3151b1bd2da48aa5d9174a851809a24d817d91a
Gabriel Da Silva Souza <gabriel.ssouza@totvs.com.br>
2020-10-27 21:47:51 -0300

    Initial Interface modelation

--- aps-2020-segundo-semestre/src/main/java/com/aps/cc/unip/DAO/HibernateConnection.java
+++ aps-2020-segundo-semestre/src/main/java/com/aps/cc/unip/DAO/HibernateConnection.java
@@ -1,28 +1,28 @@
 package com.aps.cc.unip.DAO;
 
-import com.aps.cc.unip.model.Author;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-
-public class HibernateConnection {
-    private static SessionFactory sessionFactory;
-    public HibernateConnection() {
-    }
-    public Session openSession() {
-        if (sessionFactory == null) {
-            criaSessionFactory();
-        }
-        return sessionFactory.openSession();
-    }
-    public void criaSessionFactory() {
-        Configuration configuration = new Configuration();
-        configuration.addAnnotatedClass(Author.class);
-        ServiceRegistry serviceRegistry = new
-                StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).
-                build();
-        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-    }
-}
+//import com.aps.cc.unip.model.Author;
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+//import org.hibernate.cfg.Configuration;
+//import org.hibernate.service.ServiceRegistry;
+//
+//public class HibernateConnection {
+//    private static SessionFactory sessionFactory;
+//    public HibernateConnection() {
+//    }
+//    public Session openSession() {
+//        if (sessionFactory == null) {
+//            criaSessionFactory();
+//        }
+//        return sessionFactory.openSession();
+//    }
+//    public void criaSessionFactory() {
+//        Configuration configuration = new Configuration();
+//        configuration.addAnnotatedClass(Author.class);
+//        ServiceRegistry serviceRegistry = new
+//                StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).
+//                build();
+//        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+//    }
+//}
--- aps-2020-segundo-semestre/src/main/java/com/aps/cc/unip/Main.java
+++ aps-2020-segundo-semestre/src/main/java/com/aps/cc/unip/Main.java
@@ -1,18 +1,13 @@
 package com.aps.cc.unip;
 
-import com.aps.cc.unip.DAO.BDAuthorsFunctions;
-import com.aps.cc.unip.DAO.JDBCConnection;
-import com.aps.cc.unip.model.Author;
+//import com.aps.cc.unip.DAO.BDAuthorsFunctions;
+//import com.aps.cc.unip.DAO.JDBCConnection;
+//import com.aps.cc.unip.model.Author;
+import com.aps.cc.unip.view.MainView;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        Author author = new Author("Gabriel ", "Da Silva Souza");
-
-        BDAuthorsFunctions BDFunctions = new BDAuthorsFunctions();
-
-        BDFunctions.gravar(author);
 
         System.out.println("Finish");
     }
